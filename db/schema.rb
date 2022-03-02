098b218a4434b1b49b44e74fc04c42c7dfb3b7fd
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-03-02 18:06:22 -0500

    Added long and lat to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_162042) do
+ActiveRecord::Schema.define(version: 2022_03_02_230509) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -96,6 +96,8 @@ ActiveRecord::Schema.define(version: 2022_03_02_162042) do
     t.text "bio"
     t.string "address"
     t.boolean "seamstress", default: false
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
