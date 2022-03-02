f84e5b92d8b0549dd5316505ddb5593ddb00c6bb
Cameron Woolf <rayadevinfo@gmail.com>
2022-03-02 12:10:58 -0500

    Mapbox almost done

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get '/seamstresses', 'users#index'
+  get '/users', to: 'users#index'
 end
