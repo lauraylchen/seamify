20bcaf770358beb5f1ede4dcfd6cdbd51485b469
Cameron Woolf <rayadevinfo@gmail.com>
2022-03-02 15:37:08 -0500

    Mapbox with basic seamstress filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.all.where(seamstress: true)
 
     # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
     @markers = @users.geocoded.map do |user|
