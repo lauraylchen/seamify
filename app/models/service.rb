10d646c3265cad45161bc5d65fff136413c65267
akhyra renomeron <khyrarenomeron@gmail.com>
2022-03-02 17:31:08 -0500

    searchform

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,6 @@
 class Service < ApplicationRecord
   has_many :seamstress_services
+  categories = ["dress", "t-shirt", "suit", "outerwear", "jeans", "skirt", "wedding dress", "activewear", "childwear", "swimwear"]
+  repairs = ["hemming", "zipper", "elastic", "button", "fitting", "patching"]
+  materials = ["chiffron", "cotton", "crepe", "denim", "lace", "linen", "leather", "polyester","satin", "silk", "suede", "nylon"]
 end
