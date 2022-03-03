14c6f2675e6de74d66b36af30828eaab275d910e
akhyra renomeron <khyrarenomeron@gmail.com>
2022-03-03 10:40:31 -0500

    addd routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+  # Search Form
+  get '/search', to: 'pages#search', as: 'search_path'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # View all Seamstresses
