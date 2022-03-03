2466f2434e6b7e7607344bd3968820c77cc189f2
Isabelle Vallerand <isabelle.vall@gmail.com>
2022-03-03 12:02:31 -0500

    fixed db

--- db/migrate/20220228223516_create_order_items.rb
+++ db/migrate/20220228223516_create_order_items.rb
@@ -2,7 +2,6 @@ class CreateOrderItems < ActiveRecord::Migration[6.1]
   def change
     create_table :order_items do |t|
       t.references :order, null: false, foreign_key: true
-      t.references :seamstress_service, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20220303042733_remove_reference_from_order_items.rb
+++
@@ -1,6 +0,0 @@
-class RemoveReferenceFromOrderItems < ActiveRecord::Migration[6.1]
-  def change
-    remove_index :order_items, :seamstress_service_id
-    remove_column :order_items, :seamstress_service_id, :bigint
-  end
-end
