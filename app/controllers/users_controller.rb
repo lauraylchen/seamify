f2133aac8d3f526c099279344fcfafc8bf1bcff9
Cameron Woolf <rayadevinfo@gmail.com>
2022-03-04 13:17:41 -0500

    Search function progress

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,8 @@ class UsersController < ApplicationController
       @users = User.all.where(seamstress: true)
     end
 
-    # @users
-    # raise
-    @users = search
-  
-    # raise
+    # @users = search
+
 
     # @users = @users_Array
     # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
@@ -34,52 +31,27 @@ class UsersController < ApplicationController
   end
 
   def search
+    # If you start by search bar, only filter by locatiom
+    # If you start in start journey, filter by location and services!
 
+    # Needed!!!
     @search = Service.new
     @clothings = Service.clothings
     @repairs = Service.repairs
     @materials = Service.materials
-  
-    # If statements allow for search if user does not select anything!
-    # if params[:clothings] == nil || params[:clothing] == [] 
-    #   @services = Service.all
-    # else 
-    #   @services = Service.where(clothing: params[:clothings])
-    # end
 
-    # if params[:repairs] == nil || params[:clothing] == [] 
-    #   @services = Service.all
-    # else
-    #   @services = Service.where(repair: params[:repairs])
-    # end
-
-    # if params[:materials] == nil || params[:materials] == [] 
-    #   @services = Service.all
-    # else
-    #   @services = Service.where(material: params[:materials])
-    # end
-
-    if params[:query] != nil
-      @services = Service.where(params[:query])
+    if params[:query].present?
+      @users = User.near(params[:query], 10)
     else
-      @services = Service.all
+      @users = User.all.where(seamstress: true)
     end
     
+    @services = Service.where(clothing: params[:clothings], repair: params[:repairs], material: params[:materials] )
+   
     test1 = @services.pluck(:seamstress_id).uniq
     test2 = User.where(id: test1)
-    raise
-
     # raise
 
-    # user_Array = []
-    # @services.each do |service|
-    #   user = User.find(service.seamstress.id)
-    #   user_Array.push(user)
-    # end
-
-    # Search where users = id1 or id2 or id3
-    # raise
-    # return user_Array
-
+    
   end
 end
