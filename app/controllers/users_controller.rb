e734336a7cc6855464c9afe27ff97edf9f020df1
Cameron Woolf <rayadevinfo@gmail.com>
2022-03-04 12:50:38 -0500

    Fixing search

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,21 @@ class UsersController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index]
 
   def index
+
+    # Location search
     if params[:query].present?
       @users = User.near(params[:query], 10)
     else
       @users = User.all.where(seamstress: true)
     end
 
+    # @users
+    # raise
+    @users = search
+  
+    # raise
+
+    # @users = @users_Array
     # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
     @markers = @users.geocoded.map do |user|
       {
@@ -17,6 +26,7 @@ class UsersController < ApplicationController
         image_url: helpers.asset_url("yarn_marker.png")
       }
     end
+
   end
 
   def show
@@ -31,26 +41,45 @@ class UsersController < ApplicationController
     @materials = Service.materials
   
     # If statements allow for search if user does not select anything!
-    if params[:clothings] == nil || params[:clothing] == [] 
-      @services = Service.all
-    else
-      @services = Service.where(clothing: params[:clothings])
-    end
+    # if params[:clothings] == nil || params[:clothing] == [] 
+    #   @services = Service.all
+    # else 
+    #   @services = Service.where(clothing: params[:clothings])
+    # end
 
-    if params[:repairs] == nil || params[:clothing] == [] 
-      @services = Service.all
-    else
-      @services = Service.where(repair: params[:repairs])
-    end
+    # if params[:repairs] == nil || params[:clothing] == [] 
+    #   @services = Service.all
+    # else
+    #   @services = Service.where(repair: params[:repairs])
+    # end
 
-    if params[:materials] == nil || params[:materials] == [] 
-      @services = Service.all
+    # if params[:materials] == nil || params[:materials] == [] 
+    #   @services = Service.all
+    # else
+    #   @services = Service.where(material: params[:materials])
+    # end
+
+    if params[:query] != nil
+      @services = Service.where(params[:query])
     else
-      @services = Service.where(material: params[:materials])
+      @services = Service.all
     end
+    
+    test1 = @services.pluck(:seamstress_id).uniq
+    test2 = User.where(id: test1)
+    raise
+
     # raise
-    
-    
-  
+
+    # user_Array = []
+    # @services.each do |service|
+    #   user = User.find(service.seamstress.id)
+    #   user_Array.push(user)
+    # end
+
+    # Search where users = id1 or id2 or id3
+    # raise
+    # return user_Array
+
   end
 end
