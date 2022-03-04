4014416548976bd37d641c8eadd90a30244b19cf
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-03-03 19:44:58 -0500

    connection to show page done

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # View all Seamstresses
-  get '/users', to: 'users#index'
+  resources :users, only: %i[index show]
 
   # Create a new user profile
   # get '/new', to: 'users#new'
