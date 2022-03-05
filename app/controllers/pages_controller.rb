0b36c2031afff24d40cd5425d8916d027f73a85d
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-03-05 16:45:58 -0500

    Decided on the color banner and made the meet card dynamic

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @seamstresses = []
+    @seamstresses.push(User.where(first_name: "Yolanda").first)
+    @seamstresses.push(User.where(first_name: "Giuseppe").first)
+    @seamstresses.push(User.where(first_name: "Rey").first)
   end
-
 end
