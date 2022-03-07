eefcf1d45376c93b54ee9408ef055c7fc8c8971a
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-03-07 17:58:19 -0500

    Added dependencies to the models

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :orders_as_client, class_name: "Order", foreign_key: :client_id
-  has_many :orders_as_seamstress, class_name: "Order", foreign_key: :seamstress_id
-  has_many :services, class_name: "Service", foreign_key: :seamstress_id
+  has_many :orders_as_client, class_name: "Order", foreign_key: :client_id, dependent: :destroy
+  has_many :orders_as_seamstress, class_name: "Order", foreign_key: :seamstress_id, dependent: :destroy
+  has_many :services, class_name: "Service", foreign_key: :seamstress_id, dependent: :destroy
 
-  has_many :conversations_as_client, class_name: "Conversation", foreign_key: :client_id
-  has_many :conversations_as_seamstress, class_name: "Conversation", foreign_key: :seamstress_id
-  has_many :messages, class_name: "Message", foreign_key: :sender_id
+  has_many :conversations_as_client, class_name: "Conversation", foreign_key: :client_id, dependent: :destroy
+  has_many :conversations_as_seamstress, class_name: "Conversation", foreign_key: :seamstress_id, dependent: :destroy
+  has_many :messages, class_name: "Message", foreign_key: :sender_id, dependent: :destroy
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
