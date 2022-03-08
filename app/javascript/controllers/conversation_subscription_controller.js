3e25bc265150741d8ce0590f27b06e908b70deb7
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-03-08 12:37:36 -0500

    Fixed the javascript

--- app/javascript/controllers/conversation_subscription_controller.js
+++ app/javascript/controllers/conversation_subscription_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 import consumer from "../channels/consumer"
 
 export default class extends Controller {
-  static values = { conversationId: Number }
+  static values = { conversationId: Number, userId: Number }
   static targets = ["messages", "form"]
 
   connect() {
@@ -20,6 +20,18 @@ export default class extends Controller {
   }
 
   #insertMessageScrollDownAndResetForm(data) {
+    var htmlObject = document.createElement('div');
+    htmlObject.innerHTML = data;
+    var firstDiv = htmlObject.firstChild;
+    console.log(firstDiv.id);
+    console.log(this.userIdValue);
+    if (firstDiv.id != this.userIdValue) {
+      var lines = data.split("\n")
+      lines[0] = "  <div>"
+      lines[1] = "    <div class=\"message-bubble\">"
+      data = lines.join("\n")
+    }
+
     this.messagesTarget.insertAdjacentHTML("beforeend", data)
     this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
     this.formTarget.reset()
